0c8d6a205c626dea62d16ad153b32173e480564c
EthanZhang26 <ethanzca26@gmail.com>
2022-12-23 17:57:40 -0800

    Added constructor

--- src/main/java/dna/DNA.java
+++ src/main/java/dna/DNA.java
@@ -1,2 +1,9 @@
 // TODO: Implement the DNA datatype from scratch!
 // Use the test cases to guide you.
+package dna;
+public class DNA {
+    private String sequence;
+    public DNA(String sequence){
+        this.sequence = sequence;
+    }
+}
