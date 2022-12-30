a1aba94f8dc76c9f1b5e78c5a0a35fcad21304cb
Ethanzhang26 <ethanzca26@gmail.com>
2022-12-30 15:30:56 -0800

    Dec 30

--- src/main/java/dna/DNA.java
+++ src/main/java/dna/DNA.java
@@ -1,9 +1,55 @@
 // TODO: Implement the DNA datatype from scratch!
 // Use the test cases to guide you.
 package dna;
+
 public class DNA {
-    private String sequence;
-    public DNA(String sequence){
+    private final String sequence;
+    private String noJunkSequence;
+    private int numOfJunk;
+
+    public DNA(String sequence) {
         this.sequence = sequence;
+        removeJunk();
+    }
+
+    public boolean isProtein() {
+        //(use substring)if codon start with ATG,
+        if(!noJunkSequence.substring(0, 3).equals("ATG")) {
+            return false;
+        }
+        if(!noJunkSequence.substring(noJunkSequence.length()-3, noJunkSequence.length()).equals("TAA")
+        || !noJunkSequence.substring(noJunkSequence.length()-3, noJunkSequence.length()).equals("TAG")
+        || !noJunkSequence.substring(noJunkSequence.length()-3, noJunkSequence.length()).equals("TGA")){
+
+        }
+
+        //if codon end with TAA, TAG, or TGA
+        //
+    }
+
+    public void removeJunk() {
+        noJunkSequence = "";
+        numOfJunk = 0;
+
+        //remove junk from sequence and save to noJunkSequence
+        //count the number of junk and save in numOfJunk
+        for (int i = 0; i < sequence.length(); i++) {
+            //check every index in sequence
+            //check if equals to A, T, G, or C
+            if (sequence.charAt(i) == 'A'
+                    || sequence.charAt(i) == 'T'
+                    || sequence.charAt(i) == 'G'
+                    || sequence.charAt(i) == 'C') {
+                noJunkSequence = noJunkSequence + sequence.charAt(i);
+
+            } else {
+                numOfJunk = numOfJunk + 1;
+            }
+
+        }
+
+
+        //if true, save in noJunkSequence
+        //if false, +1 to numOfJunk
     }
 }
